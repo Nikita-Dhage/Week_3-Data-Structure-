82ad4cabf3673092ceb0023ebc2cb7cd79bdbf89
Nikita-Dhage <nikitadhage2019@gmail.com>
2025-05-01 14:06:13 +0530

    Added

---
+++ Array_delete/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] oldArray = { 10, 20, 30, 40 };
+int[] newArray = new int[oldArray.Length - 1];
+//specific position//
+int position = 2; 
+
+int j = 0;
+
+for (int i = 0; i < oldArray.Length; i++)
+{
+    if (i != position)
+    {
+        newArray[j] = oldArray[i];
+        j++;
+    }
+}
+
+//-------------from last position---------------------//
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = oldArray[i];
+}
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
---
+++ Array_insert/Program.cs
@@ -0,0 +1,52 @@
+﻿int[] oldArray = { 10, 20, 30 }; 
+int newValue = 5;
+int[] newArray = new int[oldArray.Length + 1];
+
+//-----------At the begining place element------------------//
+newArray[0] = newValue;
+
+for (int i = 0; i < oldArray.Length; i++)
+{
+    newArray[i + 1] = oldArray[i];
+}
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+
+//-------------At the specific position---------------//
+int position = 2;
+for (int i = 0; i < position; i++)
+{
+    newArray[i] = oldArray[i];
+}
+
+newArray[position] = newValue;
+for (int i = position; i < oldArray.Length; i++)
+{
+    newArray[i + 1] = oldArray[i];
+}
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+
+
+//--------------------At the ending-----------------------//
+
+
+
+for (int i = 0; i < oldArray.Length; i++)
+{
+    newArray[i] = oldArray[i];
+}
+
+
+newArray[newArray.Length - 1] = newValue;
+
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
