e9a96b49627fd5e0e3930e2fbac24331ec14e2b4
Nikita-Dhage <nikitadhage2019@gmail.com>
2025-05-05 14:40:25 +0530

    Added Content

---
+++ Queue_LinkedList/Program.cs
@@ -0,0 +1,95 @@
+﻿LinkedQueue queue = new LinkedQueue();
+
+queue.Enqueue(10);
+queue.Enqueue(20);
+queue.Enqueue(30);
+
+queue.Show();   
+
+queue.Peek();   
+
+queue.Dequeue();
+
+queue.Show();
+
+class QueueNode
+{
+    public int Value;
+    public QueueNode Next;
+
+    public QueueNode(int val)
+    {
+        Value = val;
+        Next = null;
+    }
+}
+class LinkedQueue
+{
+    private QueueNode front;
+    private QueueNode rear;
+    public void Enqueue(int number)
+    {
+        QueueNode newNode = new QueueNode(number);
+
+        if (rear == null)
+        {
+            front = rear = newNode;
+        }
+        else
+        {
+            rear.Next = newNode;
+            rear = newNode;
+        }
+
+        Console.WriteLine(number + " added to the queue.");
+    }
+    public void Dequeue()
+    {
+        if (front == null)
+        {
+            Console.WriteLine("Queue is empty. Nothing to dequeue.");
+            return;
+        }
+
+        Console.WriteLine(front.Value + " removed from the queue.");
+        front = front.Next;
+
+        if (front == null)
+        {
+            rear = null;
+        }
+    }
+    public void Peek()
+    {
+        if (front == null)
+        {
+            Console.WriteLine("Queue is empty.");
+            return;
+        }
+
+        Console.WriteLine("Front of queue: " + front.Value);
+    }
+    public bool IsEmpty()
+    {
+        return front == null;
+    }
+    public void Show()
+    {
+        if (front == null)
+        {
+            Console.WriteLine("Queue is empty.");
+            return;
+        }
+
+        Console.WriteLine("Queue elements :");
+
+        QueueNode current = front;
+        while (current != null)
+        {
+            Console.Write(current.Value + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
---
+++ Stack_LinkedList/Program.cs
@@ -0,0 +1,75 @@
+﻿LinkedStack stack = new LinkedStack();
+
+stack.Push(10);
+stack.Push(20);
+stack.Push(30);
+stack.Show();   
+stack.Peek();   
+stack.Pop();    
+stack.Show();   
+class StackNode
+{
+    public int Value;
+    public StackNode Next;
+
+    public StackNode(int val)
+    {
+        Value = val;
+        Next = null;
+    }
+}
+class LinkedStack
+{
+    private StackNode topNode;
+    public void Push(int number)
+    {
+        StackNode newNode = new StackNode(number);
+        newNode.Next = topNode; 
+        topNode = newNode;      
+        Console.WriteLine(number + " pushed onto the stack.");
+    }
+    public void Pop()
+    {
+        if (topNode == null)
+        {
+            Console.WriteLine("Stack is empty. Nothing to pop.");
+            return;
+        }
+
+        Console.WriteLine(topNode.Value + " popped from the stack.");
+        topNode = topNode.Next; 
+    }
+    public void Peek()
+    {
+        if (topNode == null)
+        {
+            Console.WriteLine("Stack is empty.");
+            return;
+        }
+
+        Console.WriteLine("Top of stack: " + topNode.Value);
+    }
+    public bool IsEmpty()
+    {
+        return topNode == null;
+    }
+    public void Show()
+    {
+        if (topNode == null)
+        {
+            Console.WriteLine("Stack is empty.");
+            return;
+        }
+
+        Console.WriteLine("Stack elements:");
+
+        StackNode current = topNode;
+        while (current != null)
+        {
+            Console.Write(current.Value + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
